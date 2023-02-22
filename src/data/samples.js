bde2295387289edbfe86f1d1122757073e6f634d
r3mot <cmendrella1@toromail.csudh.edu>
2023-02-22 06:59:39 -0800

    Add samples

---
+++ src/data/samples.js
@@ -0,0 +1,13 @@
+import hatCl from "../assets/hat-cl.wav";
+import hatOp from "../assets/hat-op.wav";
+import kick from "../assets/kick.wav";
+import snare from "../assets/snare.wav";
+import shaker from "../assets/shaker.wav";
+
+export const samples = [
+  { id: 0, name: "hat-cl", path: hatCl },
+  { id: 1, name: "hat-op", path: hatOp },
+  { id: 2, name: "kick", path: kick },
+  { id: 3, name: "snare", path: snare },
+  { id: 4, name: "shaker", path: shaker },
+];
