e16141f17a4a5814d54f404dd2387541d1669b3a
r3mot <cmendrella1@toromail.csudh.edu>
2023-02-22 07:19:36 -0800

    BPM Control component with styles

---
+++ src/components/controls/bpm/ControlBPM.jsx
@@ -0,0 +1,27 @@
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+import { useBPM } from "../../context/BPMProvider";
+import styles from "./ControlBPM.module.css";
+
+export const ControlBPM = () => {
+  const { bpm, raiseTempo, lowerTempo, mouseUp } = useBPM();
+
+  return (
+    <div className={styles.wrapper}>
+      <AiFillCaretLeft
+        size={35}
+        className={styles.button}
+        onMouseDown={lowerTempo}
+        onMouseUp={mouseUp}
+      />
+      <div className={styles.box}>
+        <h2>{bpm}BPM</h2>
+      </div>
+      <AiFillCaretRight
+        size={35}
+        className={styles.button}
+        onMouseDown={raiseTempo}
+        onMouseUp={mouseUp}
+      />
+    </div>
+  );
+};
