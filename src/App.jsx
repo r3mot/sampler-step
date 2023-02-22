3c156f77f2c49ac8745ab5a8ed1ec11476813044
r3mot <cmendrella1@toromail.csudh.edu>
2023-02-22 07:12:34 -0800

    Wrap components in context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Sequencer } from "./components";
+import { SequenceContextProvider } from "./context";
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <SequenceContextProvider>
+        <Sequencer />
+      </SequenceContextProvider>
+    </div>
+  );
 };
 
 export default App;
