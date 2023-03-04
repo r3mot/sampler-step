b74aaf1792825432221df3a5c538a8e5c1fda514
r3mot <cmendrella1@toromail.csudh.edu>
2023-03-04 11:57:56 -0800

    Refactor legend

--- src/components/legend/Legend.jsx
+++ src/components/legend/Legend.jsx
@@ -1,32 +1,10 @@
-import { useState, useEffect } from "react";
-import styles from "./Legend.module.css";
+import { useSequenceContext } from "../../context";
 import { samples } from "../../data";
-import { useSequenceContext } from "../../context/SequenceProvider";
+import { MuteButton } from "./components";
+import styles from "./Legend.module.css";
 
 export const Legend = () => {
   const { samplers } = useSequenceContext();
-
-  const [muted, setMuted] = useState(
-    new Map(samplers.current.map((sampler) => [sampler.id, sampler.muted]))
-  );
-
-  const [curr, setCurr] = useState(0);
-
-  function muteSample(sample) {
-    samplers.current[sample.id].sampler.volume.mute =
-      !samplers.current[sample.id].sampler.volume.mute;
-
-    setCurr(sample.id);
-
-    setMuted((prev) => {
-      const newMuted = new Map(prev);
-      newMuted.set(sample.id, !newMuted.get(sample.id));
-      return newMuted;
-    });
-  }
-
-  useEffect(() => {}, [muted]);
-
   return (
     <div className={styles.wrapper}>
       {samples.map((sample) => (
@@ -35,28 +13,10 @@ export const Legend = () => {
             <h1>{sample.name}</h1>
           </div>
           <div className={styles.right}>
-            <button onClick={() => muteSample(sample)}>
-              {muted.get(sample.id) ? "Unmute" : "Mute"}
-            </button>
+            <MuteButton sampler={samplers.current[sample.id].sampler} />
           </div>
         </div>
       ))}
     </div>
   );
-  // return (
-  //   <div className={styles.wrapper}>
-  //     {samples.map((sample) => (
-  //       <div key={sample.id} className={styles.track}>
-  //         <div className={styles.left}>
-  //           <h1>{sample.name}</h1>
-  //         </div>
-  //         <div key={sample.id} className={styles.right}>
-  //           <button id={sample.id} onClick={(e) => muteSample(e.target)}>
-  //             {}
-  //           </button>
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 };
