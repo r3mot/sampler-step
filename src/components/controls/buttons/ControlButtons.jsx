d9ab5ca9aad5b65bdf931e579eb7d560142275eb
r3mot <cmendrella1@toromail.csudh.edu>
2023-03-18 09:25:46 -0700

    Refactor controls component

--- src/components/controls/buttons/ControlButtons.jsx
+++ src/components/controls/buttons/ControlButtons.jsx
@@ -7,7 +7,11 @@ import styles from "./ControlButtons.module.css";
  * @returns {JSX.Element} ControlButtons Component
  */
 export const ControlButtons = () => {
-  const { start, stop, clear, isPlaying, isCleared, isStopped } = useControls();
+  const { start, stop, clear, controls } = useControls();
+
+  const getButtonStyle = (state, color) => ({
+    ...(state ? { color, border: `2px solid ${color}` } : {}),
+  });
 
   /**
    * For each button, the color and border change when the button is clicked.
@@ -18,51 +22,20 @@ export const ControlButtons = () => {
         size={35}
         className={styles.button}
         onClick={start}
-        style={
-          isPlaying ? { color: "#0fd619", border: " 2px solid #0fd619" } : ""
-        }
+        style={getButtonStyle(controls.isPlaying, "#0fd619")}
       />
       <IoStop
         size={35}
-        style={
-          isStopped
-            ? {
-                color: " rgb(196, 29, 29)",
-                border: "2px solid  rgb(196, 29, 29)",
-              }
-            : ""
-        }
         className={styles.button}
         onClick={stop}
+        style={getButtonStyle(controls.isStopped, "rgb(196, 29, 29)")}
       />
       <BiReset
         size={35}
-        style={
-          isCleared
-            ? {
-                color: " rgb(219, 231, 109)",
-                border: "2px solid  rgb(219, 231, 109)",
-              }
-            : ""
-        }
         className={styles.button}
         onClick={clear}
+        style={getButtonStyle(controls.isCleared, "rgb(219, 231, 109)")}
       />
     </div>
-  );
-};
-
-/**
- * Gradient for react icons
- * @returns
- */
-const GreenGradient = () => {
-  return (
-    <svg width='0' height='0'>
-      <radialGradient id='green-gradient' x1='100%' y1='100%' x2='0%' y2='0%'>
-        <stop stopColor='#cdf583' offset='0%' />
-        <stop stopColor='#06990d' offset='100%' />
-      </radialGradient>
-    </svg>
   );
 };
