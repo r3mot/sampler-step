8ae66aaa6f92fd561f169bbd93f0b0343581a2f8
r3mot <cmendrella1@toromail.csudh.edu>
2024-09-29 08:29:19 -0700

    chore: migrate setup.js to TypeScript with temporary any types

--- src/utility/setup.ts
+++ src/utility/setup.ts
@@ -4,7 +4,7 @@ import * as Tone from "tone";
  * Initializes the Tone.js sequencer
  * @param {*} samples - The samples to be used in the sequence
  */
-const sampler = (sample) => {
+const sampler = (sample: any) => {
   const sampler = new Tone.Sampler({
     urls: {
       ["C4"]: sample.path,
@@ -20,8 +20,8 @@ const sampler = (sample) => {
  *  Initializes the samplers and returns them to the caller
  * @param {*} samples - The samples to be used in the sequence
  */
-export const initSamplers = (samples) => {
-  return samples.map((sample) => ({
+export const initSamplers = (samples: any) => {
+  return samples.map((sample: any) => ({
     id: sample.id,
     sampler: new Tone.Sampler({
       urls: {
@@ -37,10 +37,10 @@ export const initSamplers = (samples) => {
  * @param {*} steps - The steps of the sequence
  * @param {*} numBeats - The number of steps in the sequence
  */
-export const initSequence = (samplers, steps, numBeats) => {
+export const initSequence = (samplers: any, steps: any, numBeats: any) => {
   return new Tone.Sequence(
     (time, step) => {
-      samplers.current.map((sample) => {
+      samplers.current.map((sample: any) => {
         if (steps.current[sample.id][step].checked) {
           sample.sampler.triggerAttack("C4", time);
         }
@@ -60,16 +60,16 @@ let prevStep = -1;
  * @uses - The id of the column is set to ${step}-${rowId}
  * @uses - prevStep is used to reset the opacity of the previous column
  */
-const showSteps = (steps, step) => {
+const showSteps = (steps: any, step: any) => {
   Object.keys(steps.current).forEach((rowId) => {
     // Reset the opacity of the previous column
     if (prevStep >= 0) {
-      document.getElementById(`${prevStep}-${rowId}`).style.opacity = 1;
-      document.getElementById(`${prevStep}-${rowId}`).style.filter = "none";
+      document.getElementById(`${prevStep}-${rowId}`)!.style.opacity = "1";
+      document.getElementById(`${prevStep}-${rowId}`)!.style.filter = "none";
     }
     // Set the opacity of the current column
-    document.getElementById(`${step}-${rowId}`).style.opacity = 0.2;
-    document.getElementById(`${step}-${rowId}`).style.filter =
+    document.getElementById(`${step}-${rowId}`)!.style.opacity = "0.2";
+    document.getElementById(`${step}-${rowId}`)!.style.filter =
       "drop-shadow(0 0 0.5rem black)";
   });
   prevStep = step;
