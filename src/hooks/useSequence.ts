9be65900e535823ef8d4642f635b4a527165e27e
r3mot <cmendrella1@toromail.csudh.edu>
2024-09-29 10:27:48 -0700

    refactor: update import path to use '@/'

--- src/hooks/useSequence.ts
+++ src/hooks/useSequence.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { SequenceContext } from "../context/SequenceProvider";
+import { SequenceContext } from "@/context/SequenceProvider";
 import * as Tone from "tone";
 
 export const useSequence = () => {
