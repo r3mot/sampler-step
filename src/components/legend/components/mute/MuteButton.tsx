be7ebf21461c08c642a98e79be84d35142c46d91
r3mot <cmendrella1@toromail.csudh.edu>
2024-09-29 10:34:29 -0700

    refactor(Legend): update import paths to use '@/'

--- src/components/legend/components/mute/MuteButton.tsx
+++ src/components/legend/components/mute/MuteButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaderInternal, StepInternal } from "../../../../types";
+import { FaderInternal, StepInternal } from "@/types";
 import styles from "./Mutebutton.module.css";
 
 interface Props {
