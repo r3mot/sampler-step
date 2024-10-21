d361fb97e5ee959b2911f54ff3fa19256c16405f
r3mot <cmendrella1@toromail.csudh.edu>
2024-10-21 11:27:35 -0700

    chore: remove jsdocs

--- src/components/controls/ControlPanel.tsx
+++ src/components/controls/ControlPanel.tsx
@@ -2,13 +2,6 @@ import { ControlBPM } from "@/components/controls/bpm/ControlBPM";
 import { ControlButtons } from "@/components/controls/buttons/ControlButtons";
 import styles from "./ControlPanel.module.css";
 
-/**
- * Control panel for the sequencer.<br>
- * Includes buttons for starting, stopping, and clearing the sequencer.<br>
- * Also includes a dropdown for selecting the number of beats in the sequence.<br>
- *
- * @returns {JSX.Element} - ControlPanel Component
- */
 export const ControlPanel = () => {
   return (
     <div className={styles.panel}>
